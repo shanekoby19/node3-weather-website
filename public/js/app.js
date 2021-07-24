b274df36dfd54ae538db2159f86e61d7f5912e0f
Shane Kobylecky <shanekoby19@Shanes-MacBook-Air.local>
2021-07-23 21:29:22 -0700

    Added new CSS to enhance the look

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 const weatherForm = document.querySelector(`form`);
 const searchInput = document.querySelector(`input`);
 const errorParagraph = document.querySelector(`.errorParagraph`);
+const forecastLocation = document.querySelector(`.forecast-location`);
+const forecastTemperature = document.querySelector(`.forecast-temperature`)
 const forecastParagraph = document.querySelector(`.forecastParagraph`);
 const weatherImg = document.querySelector(`.weather-icon`);
 
@@ -9,13 +11,20 @@ weatherForm.addEventListener(`submit`, (event) => {
     const city = searchInput.value;
 
     errorParagraph.textContent = `Loading...`;
+    forecastLocation.textContent = ``;
+    forecastTemperature.textContent = ``;
     forecastParagraph.textContent = ``;
+
 
     fetch(`/weather?address=${city}`).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 return errorParagraph.textContent = data.error;
             }
+            console.log(data);
+
+            forecastLocation.textContent = `${data.location}, ${data.state}`;
+            forecastTemperature.textContent = `${data.temperature} ℉`;
             forecastParagraph.textContent = data.forecast;
             searchInput.value = ``;
             errorParagraph.textContent = ``;
